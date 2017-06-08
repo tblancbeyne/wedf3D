c28a57d9c58546d9489fc378713e2d24d1913426
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-06-08 13:57:27 +0200

    Replaced some [] by () ...

--- computeFaceClustering.m
+++ computeFaceClustering.m
@@ -47,7 +47,7 @@ for i=1:size(faces,1)
     if currClust(1) ~= currClust(2) || currClust(1) ~= currClust(3)
         [faces,vertices,faceClustering] = smoothClustering(faces,i,vertices,faceClustering,currClust);
     else
-        faceClustering(i) = currClust[1];
+        faceClustering(i) = currClust(1);
     end
 
     % Mean
