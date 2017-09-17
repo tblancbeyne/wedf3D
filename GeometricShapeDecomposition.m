8d8ccd527b136ade5ae0c1852b80028aa8361f9f
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-09-17 19:00:01 +0200

    Some small modifications

--- GeometricShapeDecomposition.m
+++ GeometricShapeDecomposition.m
@@ -7,22 +7,22 @@ prompt = 'What is the number of cluster? ';
 clustNb = input(prompt);
 
 % Parsing the shape
-[verticesS,facesS] = parseOff('data/ant1/ant1.off');
+[verticesS,facesS] = parseOff('data/horse2/horse2.off');
 
 % noise = rand(size(verticesS))/10;
 % verticesS = verticesS + noise - mean(noise(:));
 
 % Parsing the medial axis
-[verticesM] = parseOff2('data/ant1/ant1-medial_axis.off');
+[verticesM] = parseOff2('data/horse2/horse2-medial_axis.off');
 
 
 % Parsing erosion thickness. In our case, the ET is in fact the MBT because
 % radiuses of balls is set to 0 when computing the ET
-verticesEt = parseEt('data/ant1/ant1-erosion_thickness.et');
+verticesEt = parseEt('data/horse2/horse2-erosion_thickness.et');
 verticesM = horzcat(verticesM,verticesEt);
 
 % Parsing skeleton
-[verticesSk,edgesSk] = parseSk2('data/ant1/ant1-skeleton.sk');
+[verticesSk,edgesSk] = parseSk2('data/horse2/horse2-skeleton.sk');
 
 %displaySkeleton(verticesSk,edgesSk);
 
@@ -56,13 +56,13 @@ toc
 % while ~isempty(edfEndPoints) && ~isempty(edfEdgesSk)
 %     minEndPoint = edfEndPoints(1,1); % Finding the point with smallest WEDF value among endpoints
 %     [edfChild] = findChild(edfEdgesSk,minEndPoint(1));  % Finding the child of this point
-%     
+%
 %     % If the point is not a junction or an endpoint (if it is, we finished
 %     % the branch). A junction point is a point that is related to THREE or more points
 %     if ~isempty(edfChild) && ~isJunctionPoint(edfChild,edfEdgesSk) && ~isEndPoint(edfChild(1),edfEdgesSk)
 %         % Computing WEDF
-%         EDF(edfChild(1)) = computeEDF(verticesSk(edfChild(1),:),verticesSk(minEndPoint(1),:),EDF(minEndPoint(1))); 
-%         
+%         EDF(edfChild(1)) = computeEDF(verticesSk(edfChild(1),:),verticesSk(minEndPoint(1),:),EDF(minEndPoint(1)));
+%
 %         % The child is a new endpoint as its parent will be deleted : it
 %         % allows junctions points to become regular point as their
 %         % connection with the parent is deleted
@@ -77,10 +77,10 @@ toc
 %     % junction point)
 %     edfEndPoints = removePoint(minEndPoint(1),edfEndPoints);
 %     edfEdgesSk = updateSkeleton(minEndPoint(1),edfEdgesSk,[]);
-%     
+%
 %     edfEndPoints = sortrows(edfEndPoints, 2);
 % end
-% 
+%
 % toc
 
 % Computing WEDF on other points
@@ -89,33 +89,33 @@ currEdgesSk = edgesSk; % Temp parameter to update the edges
 while ~isempty(endPoints) && ~isempty(currEdgesSk)
     minEndPoint = endPoints(1,1); % Finding the point with smallest WEDF value among endpoints
     [currChild] = findChild(currEdgesSk,minEndPoint(1)); % Finding the child of this point
-    
+
     % If the child exist and is not a junction nor another endpoint. A
     % junction point is a point that is related to THREE or more points
     if ~isempty(currChild) && ~isJunctionPoint(currChild,currEdgesSk) && ~isEndPoint(currChild(1),currEdgesSk)
-        
+
         % Normal WEDF computation of the child + summing the volume at junctions
         WEDF(currChild(1)) = computeWEDF(links{currChild(1)},tetrahedralization,WEDF(minEndPoint(1)));
-        WEDF(currChild(1)) = WEDF(currChild(1)) + junctionWEDF(currChild); 
+        WEDF(currChild(1)) = WEDF(currChild(1)) + junctionWEDF(currChild);
 
         % The child is a new endpoint as its parent will be deleted : it
         % allows junctions points to become regular point as their
         % connection with the parent is deleted
         endPoints = vertcat(endPoints,[currChild(1) WEDF(currChild(1))]);
-        
+
     % if the child exists but is a junction point, we had the WEDF value of
     % its current parent for later
     elseif ~isempty(currChild) && isJunctionPoint(currChild,currEdgesSk)
          junctionWEDF(currChild) = junctionWEDF(currChild) + WEDF(minEndPoint(1));
     end
-    
+
     % In all cases, we remove the current endpoint and all the edges that
     % are related to this point (so it child really becomes an endpoint if
     % it was not a junction point, or lose one connection if it was a
     % junction point)
     endPoints = removePoint(minEndPoint(1),endPoints);
     currEdgesSk = updateSkeleton(minEndPoint(1),currEdgesSk);
-    
+
     % Sorting the endpoints by WEDF value : smallest WEDF value first
     endPoints = sortrows(endPoints, 2);
 end
@@ -134,7 +134,7 @@ toc
 
 % Clustering the other skeleton points
 if ~isempty(ics)
-    [clustering,centroids] = clusterSkeleton(initClustering,centroids,ics,clustNb); 
+    [clustering,centroids] = clusterSkeleton(initClustering,centroids,ics,clustNb);
 else
     clustering = initClustering;
 end
@@ -161,34 +161,10 @@ printSkeleton(verticesS,facesS,verticesSk,clustering(:,3))
 toc
 
 % Print the clustering of the shape
-figure; 
+figure;
 hold on
 for i=1:size(facesS,1)
     fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),faceClustering(i,:),'EdgeColor','none');
 end
-
-pause;
-
-for k = 1:3;
-figure; 
-hold on
-    for i=1:size(facesS,1)
-        if faceClustering(i,:) <= k
-            fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),faceClustering(i,:),'EdgeColor','none');
-        end
-    end
-end
- 
-for k = 2:3;
-figure; 
-hold on
-    for i=1:size(facesS,1)
-        if faceClustering(i,:) <= k
-            fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[1 1 0],'EdgeColor','none');
-        end
-    end
-end
-
-disp('FIN.');
 
 end
--- clusterSkeleton.m
+++ clusterSkeleton.m
@@ -11,29 +11,31 @@ mainShapeCentroid = centroids(2);
 % Determining the points in the ics that do noot belong to the main shape
 noncoreICS = determineNonCoreICS(noncore,ics);
 
+realClusterNumber = min(clusterNumber-1,length(noncoreICS));
+
 % Dividing the non belonging to main shape ics points WEDF values to
 % perform the Kmeans clustering with initial seeds
 % First : dividing the points to cluster into optimalK bins
-percentiles = zeros(clusterNumber,1);
+percentiles = zeros(realClusterNumber,1);
 
 percentiles(1) = min(noncoreICS(:,2));
 
-for i=1:clusterNumber-2
-    percentiles(i+1) =  prctile(noncoreICS(:,2),100.0/(clusterNumber-2)*i);
+for i=1:realClusterNumber-2
+    percentiles(i+1) =  prctile(noncoreICS(:,2),100.0/(realClusterNumber-2)*i);
 end
 
 % Sometimes 100.0/(clusterNumber-1)*(clusterNumber-1) is a bit greater than 100
 % because of the machine precision
-percentiles(clusterNumber) =  prctile(noncoreICS(:,2),100.0);
+percentiles(realClusterNumber) =  prctile(noncoreICS(:,2),100.0);
 
 % Second : computing the seeds that will be used in the Kmeans clustering
-seeds = zeros(clusterNumber-1,1);
-for i=1:size(clusterNumber-1,1)
+seeds = zeros(realClusterNumber-1,1);
+for i=1:size(realClusterNumber-1,1)
     seeds(i) = (percentiles(i) + percentiles(i+1))/2;
 end
 
 % Performin kmeans clustering
-[~,centroids] = kmeans(noncoreICS(:,2),clusterNumber-1,'Start',seeds);
+[~,centroids] = kmeans(noncoreICS(:,2),realClusterNumber-1,'Start',seeds);
 
 % Sorting the centroids by increasing value
 centroids = [sort(centroids);mainShapeCentroid];
