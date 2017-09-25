tblancbeyne/wedf3D

commit 3c46a9b26f8a93498644d0fa344c7292b9dc83b5

Simplified some code

printSkeleton.m
@@ -1,11 +1,8 @@
 function [] = printSkeleton(verticesS,facesS,verticesSk,WEDF)
 
 fig = figure;
-for i=1:size(verticesSk,1)
-    hold on
-    scatter3(verticesSk(i,1),verticesSk(i,2),verticesSk(i,3),8,WEDF(i),'LineWidth',10);
-end
 hold on
-for i=1:size(facesS,1)
-    fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
-end
+scatter3(verticesSk(:,1),verticesSk(:,2),verticesSk(:,3),8,WEDF(:),'LineWidth',10);
+
+hold on
+fill3(verticesS(facesS(:,1:3),1),verticesS(facesS(:,1:3),2),verticesS(facesS(:,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);

parseOff.m
@@ -91,7 +91,7 @@ end
 % Increasing all the values by 1 to fit with matlab arrays
 edges = edges +1;
 faces = faces +1;
-edges = countEdgesAppearances(edges);
+edges = unique(edges);
 
 if fclose(file) ~= 0
     disp('Unexpected error. Aborted.');

commit 047d58b024d3840609fcd368338e0a20d983fdaf

Corrected a displaying bug introduced in commit 3c46a9b

printSkeleton.m
@@ -5,4 +5,6 @@ hold on
 scatter3(verticesSk(:,1),verticesSk(:,2),verticesSk(:,3),8,WEDF(:),'LineWidth',10);
 
 hold on
-fill3(verticesS(facesS(:,1:3),1),verticesS(facesS(:,1:3),2),verticesS(facesS(:,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
+for i=1:length(facesS)
+    fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
+end
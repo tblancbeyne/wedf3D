047d58b024d3840609fcd368338e0a20d983fdaf
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-09-25 11:02:16 +0200

    Corrected a displaying bug introduced in commit 3c46a9b

--- printSkeleton.m
+++ printSkeleton.m
@@ -5,4 +5,6 @@ hold on
 scatter3(verticesSk(:,1),verticesSk(:,2),verticesSk(:,3),8,WEDF(:),'LineWidth',10);
 
 hold on
-fill3(verticesS(facesS(:,1:3),1),verticesS(facesS(:,1:3),2),verticesS(facesS(:,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
+for i=1:length(facesS)
+    fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
+end
