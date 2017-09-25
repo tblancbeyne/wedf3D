74385a9d0132ecec784819940b906d70b48bb82b
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-09-25 11:57:42 +0200

    Segmentation fro skeleton branches

---
+++ branchClustering.m
@@ -0,0 +1,20 @@
+function [clusters] = branchClusters(clusters,edgesSk)
+
+branches = getBranches(edgesSk);
+
+if length(branches) == 1
+    return;
+end
+
+mainShape = find(clusters(:) == max(clusters));
+
+oldClusters = clusters;
+clusters = zeros(length(clusters),1);
+nbPrevClusters = 0;
+
+for i=1:length(branches)
+    clusters(branches{i}) = oldClusters(branches{i}) + nbPrevClusters;
+    nbPrevClusters = max(clusters);
+end
+
+clusters(mainShape(:)) = max(clusters) + 1;
