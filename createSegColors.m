3742fce6ca95e4abb57f290bef2788fd53404d87
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-10-17 08:51:50 +0200

    createSegColors.m to create colors for the segmentation

---
+++ createSegColors.m
@@ -0,0 +1,15 @@
+function [facesColors] = crateSegColors(faceSeg)
+
+colors = zeros(max(faceSeg),3)
+
+for i=1:size(colors,1)
+    colors(i,:) = rand(1,3);
+end
+
+facesColors = zeros(length(faceSeg),3)
+
+for i=1:length(faceSeg)
+    facesColors(i,:) = colors(faceSeg(i),:)
+end
+
+end
