9121942c2bb45b5a59e54e549ecf459fdbe60de6
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-10-17 08:55:30 +0200

    Access starting at 1 rather than 0

--- createSegColors.m
+++ createSegColors.m
@@ -1,15 +1,15 @@
 function [facesColors] = crateSegColors(faceSeg)
 
-colors = zeros(max(faceSeg),3)
+colors = zeros(max(faceSeg)+1,3)
 
 for i=1:size(colors,1)
     colors(i,:) = rand(1,3);
 end
 
-facesColors = zeros(length(faceSeg),3)
+facesColors = zeros(length(faceSeg),3);
 
 for i=1:length(faceSeg)
-    facesColors(i,:) = colors(faceSeg(i),:)
+    facesColors(i,:) = colors(faceSeg(i)+1,:);
 end
 
 end
