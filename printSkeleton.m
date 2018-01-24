72df385c32f1dda69a3472d6d22ccd004ece96ef
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2018-01-24 08:53:08 +0100

    Added screwdriver and teddybear shapes

--- printSkeleton.m
+++ printSkeleton.m
@@ -6,5 +6,7 @@ scatter3(verticesSk(:,1),verticesSk(:,2),verticesSk(:,3),8,WEDF(:),'LineWidth',1
 
 hold on
 for i=1:length(facesS)
-    fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.1);
+    fill3(verticesS(facesS(i,1:3),1),verticesS(facesS(i,1:3),2),verticesS(facesS(i,1:3),3),[0.8,0.8,0.8],'EdgeColor','none','FaceAlpha',0.4);
 end
+
+axis equal;
